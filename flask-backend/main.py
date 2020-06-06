54b06e7b6a7685a1b4c3804416edd1aa14e01f7b
Mathew Han <mathewjhan@gmail.com>
2020-06-06 02:04:47 -0700

    push to github

---
+++ flask-backend/main.py
@@ -0,0 +1,34 @@
+import flask
+import requests
+import time
+import random
+from cloud_api import Analyzer
+
+app = flask.Flask("__main__")
+
+@app.route("/", methods=["POST", "GET"])
+def my_index():
+    return flask.render_template("index.html")
+
+@app.route("/sentiment", methods=["POST", "GET"])
+def get_sentiment():
+    if(flask.request.method == "POST"):
+        analyzer = Analyzer()
+
+        request_json = flask.request.get_json()
+
+        chat = request_json.get("chat")
+
+        if(chat == ""):
+            return {'sentiment': 0,
+                    'verdict': "???"}
+
+        result = analyzer.sample_analyze_sentiment(chat)
+
+        return {'sentiment': result["docscore"],
+                'verdict': result["verdict"]}
+    return {'sentiment': 0,
+            'verdict': "???"}
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
---
+++ flask-backend/static/react/precache-manifest.8c1f4e6c2a11031c29665ecdc541ae0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e9406e7dd26e14ced4a60c522c3825",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "b2e2adb0171a01fdc05f",
+    "url": "/static/react/css/main.8ad62d0c.chunk.css"
+  },
+  {
+    "revision": "be89848752b8460437a2",
+    "url": "/static/react/js/2.afb11913.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.afb11913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e2adb0171a01fdc05f",
+    "url": "/static/react/js/main.0f560fa3.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
